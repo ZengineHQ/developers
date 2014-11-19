da4c3b5138030bf6ce1cc7dc2c6a6353da3d24d6
Everton Yoshitani <everton@notreve.com>
2014-11-19 17:40:55 +0900

    Redirect to HTTPS if not localhost(0.0.0.0)

--- js/redirect.js
+++ js/redirect.js
@@ -1,3 +1,5 @@
-if (window.location.protocol != "https:") {
+
+// Redirect to HTTPS if not localhost
+if (window.location.protocol != "https:" && window.location.hostname !== '0.0.0.0') {
     window.location.href = "https:" + window.location.href.substring(window.location.protocol.length);
 }
