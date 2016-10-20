68648202a9d3ca01de31643ba05feda27d834fff
AParks <thalia1.618@gmail.com>
2016-10-20 16:29:25 -0500

    remove unnecessary file, now github enforces https

--- js/redirect.js
+++
@@ -1,5 +0,0 @@
-
-// Redirect to HTTPS if not localhost
-if (window.location.protocol != "https:" && window.location.hostname !== '0.0.0.0') {
-    window.location.href = "https:" + window.location.href.substring(window.location.protocol.length);
-}
